a9528ab43b1f8ffe70c25062995559b94826d255
adrianobrito <adrianosdebrito@gmail.com>
2014-11-19 09:31:28 -0300

    Continuando testes de informacoes profissionais

--- spec/apis/v1/info_profissionais_spec.rb
+++ spec/apis/v1/info_profissionais_spec.rb
@@ -25,4 +25,32 @@ describe '/api/v1/cvs/info_profisionais', :type => :api do
 
 	end
 
+	it "deve disparar um erro ao atualizar com informações invalidas" do
+	end
+
+	it "adicionando uma informação profissional" do
+
+		info_profissional = FactoryGirl.attributes_for(:info_profissional)
+		novo_cargo = "Gari"
+
+		post "#{url}/#{info_profissional.id}.json",
+			 :info_profissional => info_profissional.merge({
+			 	:cv_id => cv.id,
+			 	:cargo => novo_cargo
+			 })
+
+		info_profissional = JSON.parse last_response.body
+
+		info_profissional["id"].should be
+		info_profissional["cargo"].should eql novo_cargo
+		last_response.status.should eql(200)
+
+	end
+
+	it "removendo uma informação profissional" do
+	end
+
+	it "obtendo uma informação profissional" do
+	end
+
 end
