f3c7884ccd65c759b5f1bd2782b848545d1cebe8
adrianobrito <adrianosdebrito@gmail.com>
2014-11-19 10:19:53 -0300

    Finalizado os testes para adicao de informacoes profissionais

--- app/controllers/api/v1/info_profissionais_controller.rb
+++ app/controllers/api/v1/info_profissionais_controller.rb
@@ -6,5 +6,26 @@ class Api::V1::InfoProfissionaisController < Api::V1::BaseController
 		respond_put(info_profissional)
 	end
 
+	def create
+		info_profissional = InfoProfissional.create(params[:info_profissional])
+		
+		if info_profissional.valid?
+			respond_with(info_profissional, :location => api_v1_cv_info_profissional_path(info_profissional.cv, info_profissional))
+		else
+			respond_with(info_profissional)
+		end
+	end
+
+	def show
+		info_profissional = InfoProfissional.find(params[:id])
+		respond_with(info_profissional)
+	end
+
+	def destroy
+		info_profissional = InfoProfissional.find(params[:id])
+		info_profissional.destroy
+
+		respond_with(info_profissional)
+	end	
 
 end
--- spec/apis/v1/info_profissionais_spec.rb
+++ spec/apis/v1/info_profissionais_spec.rb
@@ -26,31 +26,72 @@ describe '/api/v1/cvs/info_profisionais', :type => :api do
 	end
 
 	it "deve disparar um erro ao atualizar com informações invalidas" do
+		info_profissional = cv.info_profissionals[0]
+		
+		put "#{url}/#{info_profissional.id}.json", 
+			 :info_profissional => FactoryGirl.attributes_for(:info_profissional)
+			 								  .merge({
+			 								  		:id => info_profissional.id,
+			 								  		:cv_id => cv.id,
+			 								  		:empresa => nil
+			 								   })
+
+		last_response.status.should eql(422)
 	end
 
 	it "adicionando uma informação profissional" do
 
+		quantidade_antiga = InfoProfissional.where(:cv_id => cv.id).count
 		info_profissional = FactoryGirl.attributes_for(:info_profissional)
+									   .merge({:atividades_attributes => [FactoryGirl.attributes_for(:atividade)]})
 		novo_cargo = "Gari"
 
-		post "#{url}/#{info_profissional.id}.json",
+		post "#{url}.json",
 			 :info_profissional => info_profissional.merge({
 			 	:cv_id => cv.id,
 			 	:cargo => novo_cargo
 			 })
 
 		info_profissional = JSON.parse last_response.body
+		quantidade_nova = InfoProfissional.where(:cv_id => cv.id).count
 
+		quantidade_nova.should be (quantidade_antiga + 1)
 		info_profissional["id"].should be
 		info_profissional["cargo"].should eql novo_cargo
-		last_response.status.should eql(200)
+		last_response.status.should eql(201)
+	end
 
+	it "deve disparar um erro ao inserir informações invalidas" do
+		info_profissional = FactoryGirl.attributes_for(:info_profissional)
+									   .merge({:atividades_attributes => [FactoryGirl.attributes_for(:atividade)]})
+
+		post "#{url}.json",
+			 :info_profissional => info_profissional.merge({
+			 	:cv_id => cv.id,
+			 	:cargo => nil
+			 })
+
+		last_response.status.should eql(422)
 	end
 
 	it "removendo uma informação profissional" do
+		info_profissional = cv.info_profissionals[0]
+
+		delete "#{url}/#{info_profissional.id}.json"
+		info_profissional_deletada = InfoProfissional.find_by(:id => info_profissional.id)
+
+		info_profissional_deletada.should be nil
+		last_response.status.should eql(204)
 	end
 
 	it "obtendo uma informação profissional" do
+		info_profissional = cv.info_profissionals[0]
+		get "#{url}/#{info_profissional.id}.json"
+
+		info_profissional_json = JSON.parse last_response.body
+
+		last_response.status.should eql(200)
+		info_profissional_json["id"].should eql info_profissional.id
 	end
 
 end
