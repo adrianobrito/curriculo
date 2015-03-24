ebe7ee4711af83417851031225863a98b972a10f
adrianobrito <adrianosdebrito@gmail.com>
2015-03-24 11:55:22 -0300

    Back to CORS

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -2,7 +2,14 @@ class Api::V1::BaseController < ActionController::Base
 	respond_to :json
 	
 	before_filter :permitir_parametros
-	
+	before_filter :allow_ajax_request_from_other_domains
+
+	def allow_ajax_request_from_other_domains
+		headers['Access-Control-Allow-Origin'] = '*'
+		headers['Access-Control-Request-Method'] = '*'
+		headers['Access-Control-Allow-Headers'] = '*'
+	end
+  	
   	def permitir_parametros
   		params.permit!
   	end
@@ -30,7 +37,7 @@ class Api::V1::BaseController < ActionController::Base
 
     private
 	    def set_access_control_headers 
-	      headers['Access-Control-Allow-Origin'] = request.env['HTTP_ORIGIN']
+	      headers['Access-Control-Allow-Origin'] = '*'
 	      headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
 	      headers['Access-Control-Max-Age'] = '1000'
 	      headers['Access-Control-Allow-Headers'] = '*,x-requested-with'
