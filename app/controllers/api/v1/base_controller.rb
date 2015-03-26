bdb9734ed22d5be2bdd2e0ac7071bf7f4c68b3a0
adrianobrito <adrianosdebrito@gmail.com>
2015-03-26 16:20:10 -0300

    Fixing cors

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -2,15 +2,6 @@ class Api::V1::BaseController < ActionController::Base
 	respond_to :json
 	
 	before_filter :permitir_parametros
-	after_filter :cors_set_access_control_headers
-
-	# For all responses in this controller, return the CORS access control headers.
-	def cors_set_access_control_headers
-	  headers['Access-Control-Allow-Origin'] = '*'
-	  headers['Access-Control-Allow-Methods'] = 'GET, POST, OPTIONS, DELETE, PUT'
-	  headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept'
-	end
-
 
   	def permitir_parametros
   		params.permit!
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,13 +3,4 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   	protect_from_forgery with: :exception
 
-  	def options
-  		headers['Access-Control-Allow-Credentials'] = 'true'
-  		headers['Access-Control-Allow-Origin'] = '*'
-	  	headers['Access-Control-Allow-Methods'] = 'GET, POST, OPTIONS, DELETE, PUT'
-	  	headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept'
-
-	  	render :text => '', :content_type => 'text/plain'
-  	end
-
 end
